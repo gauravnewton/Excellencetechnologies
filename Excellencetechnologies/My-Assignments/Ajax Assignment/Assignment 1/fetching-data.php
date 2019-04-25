584ac50d37ceb8899c0a02c14fba9df229904a12
Gaurav <gaurav@excellencetechnologies.in>
2019-04-25 10:42:46 +0530

    Assets folder of assignment 6 updated

--- Excellencetechnologies/My-Assignments/Ajax Assignment/Assignment 1/fetching-data.php
+++ Excellencetechnologies/My-Assignments/Ajax Assignment/Assignment 1/fetching-data.php
@@ -19,11 +19,11 @@
       <table class='table table-bordered'>  
            <tr>  
                 <th width='20%'>Name</th>  
-                <th width='40%'>Emial</th>  
+                <th width='40%'>Email</th>  
                 <th width='20%'>Message</th>
                 <th width='20%'>Date</th>
            </tr>  
- ";  
+ ";  //concatinate output data
  while($row = mysqli_fetch_array($result))  
  {  
       $output .= '  
@@ -36,14 +36,14 @@
            </tr>  
       ';  
  }  
- $output .= '</table><br /><div class="panel-footer"><div align="right">';  
+ $output .= '</table><br /><div class="panel-footer"><div align="right">';  //concatinate output data
  $page_query = "SELECT * FROM userData ORDER BY id DESC";  
  $page_result = mysqli_query($connect, $page_query);  
  $total_records = mysqli_num_rows($page_result);  
  $total_pages = ceil($total_records/$record_per_page);  
  for($i=1; $i<=$total_pages; $i++)  
  {  
-      $output .= "<span class='pagination_link ' style='cursor:pointer; padding:6px; border:1px solid #ccc;' id='".$i."'>".$i."</span>";  
+      $output .= "<span class='pagination_link ' style='cursor:pointer; padding:6px; border:1px solid #ccc;' id='".$i."'>".$i."</span>";  //concatinate output data
  }  
  $output .= '</div></div><br /><br />';  
  echo $output;  
--- Excellencetechnologies/My-Assignments/Ajax Assignment/Assignment 1/index.php
+++ Excellencetechnologies/My-Assignments/Ajax Assignment/Assignment 1/index.php
@@ -3,8 +3,10 @@
       <head>  
            <title>Pagination using Jquery, PHP, Ajax and MySQL</title>  
            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />  
-           <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>  
+             
            <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>  
+
+           <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
       </head>  
       <body>  
            <br /><br />  
